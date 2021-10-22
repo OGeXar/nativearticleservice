21144b1bf517716c9b8246533ca93e4008602bad
Jan <jan0000046@gmail.com>
2021-10-22 17:53:03 +0200

    init

---
+++ nativearticleservice/src/main/java/com/ba/nativearticleservice/NativearticleserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ba.nativearticleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativearticleserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativearticleserviceApplication.class, args);
+	}
+
+}
---
+++ nativearticleservice/src/test/java/com/ba/nativearticleservice/NativearticleserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ba.nativearticleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativearticleserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
